23c1b51686518a3a13fd15b0d746de8bd9af3cdf
Taher Barwaniwala <89532500+Thorfinnn@users.noreply.github.com>
2023-02-23 07:34:00 +0000

    wrote api to return date

--- index.js
+++ index.js
@@ -12,12 +12,36 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.set('json spaces',2);
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?",(req,res)=>{
+  date = req.params.date;
+  if(date.length>11){
+    datenum = parseInt(date);
+    return res.json({
+      unix: datenum,
+      utc: new Date(datenum).toUTCString()
+    });
+  }else{
+    dateobject = new Date(date);
+    if(dateobject.toString!="Invalid Date"){
+      return res.json({
+        unix: dateobject.valueOf(),
+        utc: dateobject.toUTCString()
+      });
+    }else{
+      return res.json({
+        error: "Invalid Date"
+      });
+    }
+  }
+  
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
